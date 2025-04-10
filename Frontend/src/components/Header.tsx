b8b39115b86e1817b439d16138a0446baec0fbcc
Joshua Giron <jthomasgiron@gmail.com>
2025-04-10 14:46:20 -0300

    adding page icon

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -2,15 +2,15 @@
 function Header() {
     
     return (
-        <div className="flex items-center justify-between">
-            <h1 className="text-4xl font-bold ml-24 mt-2.5 ">
-                PlayGround
-            </h1>
-            <nav>
+        <div className="flex items-center justify-between bg-green-700 text-white">
+            <nav className="flex justify-between w-full">
+                <h1 className="text-4xl font-bold p-2.5 ml-24 ">
+                    PlayGround
+                </h1>
                 <ul className="flex items-center gap-24 text-xl mr-40">
-                    <li>Games</li>
-                    <li>Consoles</li>
-                    <li>Support</li>
+                    <li className="hover:text-gray-300 cursor-pointer">Games</li>
+                    <li className="hover:text-gray-300 cursor-pointer">Consoles</li>
+                    <li className="hover:text-gray-300 cursor-pointer">Support</li>
                 </ul>
             </nav>
         </div>
